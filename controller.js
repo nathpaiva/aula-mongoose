e7bf5db992a2952909d43628b36b6590951c504a
NathPaiva <nathalia.piccoli@e-smart.com.br>
2015-07-26 16:35:25 -0300

    ajuste na variavel

--- controller.js
+++ controller.js
@@ -24,6 +24,7 @@ var Model = require("./model"),
 			});
 		},
 		retrieve: function(req, res) {
+			var query = {};
 			Model.find(query, function (err, data) {
 				if (err){
 					console.log('Erro: ', err);
